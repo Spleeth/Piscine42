5a42b5e7710a59161cb4955ea12e937c8c4cb21b
Tony Besnard <tbesnard@c1r7p2.42lehavre.fr>
2023-07-12 18:18:43 +0200

    Ajout du C04

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbesnard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 18:10:48 by tbesnard          #+#    #+#             */
+/*   Updated: 2023/07/12 18:16:48 by tbesnard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	int	mod;
+	int nb_divided;
+
+	if(nb == -2147483648)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		mod = nb % 10;
+		nb_divided = nb / 10;
+		ft_putnbr(nb_divided);
+	}
+	if (nb > 9)
+	{
+		ft_putchar(mod + '0');
+	}
+	else
+		ft_putchar(nb + '0');
+}
