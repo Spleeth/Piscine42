9c1f8cbe0b3b9dea79f75ff41a7a82825818bf07
Tony Besnard <tbesnard@c1r6p3.42lehavre.fr>
2023-07-20 16:21:30 +0200

    C07 - ex03

---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbesnard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 15:43:16 by tbesnard          #+#    #+#             */
+/*   Updated: 2023/07/20 15:52:57 by tbesnard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*pnt;
+	int	pnt_len;
+	int	i;
+
+	if (min >= max)
+		return (NULL); 
+	pnt_len = max - min;
+	if (pnt_len < 1)
+		pnt = NULL;
+	else
+	{
+		pnt = malloc(pnt_len * sizeof(int));
+		if (!pnt)
+			return (NULL);
+		i = 0;
+		while (pnt_len > i)
+		{
+			pnt[i] = (min + i);
+			i++;
+		}
+	}
+	return (pnt);
+}
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	pnt_len;
+
+	pnt_len = max - min;
+	if (pnt_len < 1)
+	{
+		*range = NULL;
+		return (0);
+	}
+	else
+	{
+		*range = ft_range(min, max);
+		if (!range)
+		{
+			return(-1);
+		}
+	}
+	return (pnt_len);
+}
---
+++ C07/ex03/ft_strjoin.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbesnard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 15:57:17 by tbesnard          #+#    #+#             */
+/*   Updated: 2023/07/20 16:19:29 by tbesnard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+int	ft_strlen(char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		i++;
+	}
+	return(i);
+}
+
+int		return_lenght(int size, char **strs, int size_sep)
+{
+	int i;
+	int len;
+
+	i = -1;
+	len = size_sep * -1;
+	while (++i < size)
+		len += size_sep + ft_strlen(strs[i]);
+	return (len);
+}
+
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		*dest = src[i];
+		i++;
+		dest++;
+	}
+	return (dest);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	int		i;
+	char	*res;
+	int		len;
+
+	if (size == 0)
+	{
+		res = (char*)malloc(1);
+		return (res);
+	}
+	len = return_lenght(size, strs, ft_strlen(sep));
+	i = -1;
+	if ((res = malloc(sizeof(char) * (len + 1))) == NULL)
+		return (0);
+	while (++i < size)
+	{
+		res = ft_strcpy(res, strs[i]);
+		if (i + 1 < size)
+			res = ft_strcpy(res, sep);
+	}
+	*res = '\0';
+	return (res - len);
+
+}
