1bcdd1e0c18fbd029e08cb454ea2c8bde8d95b20
Tony Besnard <tbesnard@c1r7p2.42lehavre.fr>
2023-07-25 14:25:11 +0200

    RM a.out

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -38,36 +38,3 @@ int	*ft_range(int min, int max)
 	}
 	return (pnt);
 }
-
-int	main(int argc, char **argv)
-{
-	int	min;
-	int	max;
-	int	*tab;
-	int	range;
-	int	i;
-
-	if(argc > 2)
-	{
-		min = atoi(argv[1]);
-		max = atoi(argv[2]);
-		range = max - min;
-		tab = ft_range(min, max);
-		if(tab && range > 0)
-		{
-			i = 0;
-			while (range > i)
-			{
-				printf("%d", tab[i++]);
-				if ( range > i)
-					printf(", ");
-			}
-			printf("\n");
-		}
-		else
-			printf("NULL\n");
-	}
-	else
-		printf("required 'min' 'max'\n");
-	return(0);
-}
