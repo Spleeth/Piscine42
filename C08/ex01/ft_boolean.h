8079f53e857c3adee5a40440da55959f87d1b82c
Tony Besnard <tbesnard@c1r7p2.42lehavre.fr>
2023-07-26 15:03:42 +0200

    Correction C08

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -1,12 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbesnard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 14:54:22 by tbesnard          #+#    #+#             */
+/*   Updated: 2023/07/26 14:56:22 by tbesnard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
 
 # include <unistd.h>
+
+typedef char	t_bool;
+# define FALSE 0
+# define TRUE 1
+
+# define SUCCESS 0
 # define EVEN_MSG "I have an even number of arguments.\n"
 # define ODD_MSG "I have an odd number of arguments.\n"
-# define SUCCESS 0
-# define TRUE 1
-# define FALSE 0
-# define EVEN(nbr) (nbr % 2 == 0)
-typedef unsigned int	t_bool;
+
+# define EVEN(X) ((X % 2) ? 0 : 1)
+
 #endif
